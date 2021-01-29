474c79640a911a7dc4c23515b6b045f8046144eb
vun <vu_nguyen@sfu.ca>
2021-01-28 17:36:44 -0800

    Updates

---
+++ precache-manifest.ef8125a5f527cc8c75f937d9e38d778e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177b3305a099ac2ee3f79be73ef8efb3",
+    "url": "/ifarm/index.html"
+  },
+  {
+    "revision": "92f60b4d2b13ed9929ec",
+    "url": "/ifarm/static/css/main.99e75adc.chunk.css"
+  },
+  {
+    "revision": "eb0811eb271a95e5d381",
+    "url": "/ifarm/static/js/2.6f80eecb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ifarm/static/js/2.6f80eecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92f60b4d2b13ed9929ec",
+    "url": "/ifarm/static/js/main.bc71f3b0.chunk.js"
+  },
+  {
+    "revision": "a86aed3aea7ff7a4f99e",
+    "url": "/ifarm/static/js/runtime-main.44ec349a.js"
+  },
+  {
+    "revision": "973c6a12fde2400425b95f146e4fbe66",
+    "url": "/ifarm/static/media/ifarm-1.973c6a12.jpg"
+  },
+  {
+    "revision": "4e0deb73b3dcd71995303d893943d160",
+    "url": "/ifarm/static/media/ifarm-2.4e0deb73.jpg"
+  },
+  {
+    "revision": "448893c935cc57a20d72410c7ea66c04",
+    "url": "/ifarm/static/media/ifarm-3.448893c9.jpg"
+  },
+  {
+    "revision": "d9e440a5ed9283a035d51b4cb93663f0",
+    "url": "/ifarm/static/media/svg-3.d9e440a5.svg"
+  },
+  {
+    "revision": "46483eb5b9ee39b5f04f622ec6574715",
+    "url": "/ifarm/static/media/video-3.46483eb5.mp4"
+  }
+]);
